e45eaa8bd69b9b7cf7ab8386de5faa00acd1d02a
Piotr Michna <pm@piotrmichna.pl>
2020-11-28 16:33:38 +0100

    definijca magicznej metody __init__ dla DbColumn

--- create_db.py
+++ create_db.py
@@ -5,4 +5,5 @@ class DbColumn(conection_db.Connection):
     COL_TYPE = ['serial', 'integer', 'varchar', 'timestamp']
     # COLTYPE_PARAM_DICT={}
 
-    
+    def __init__(self, name, col_type, col_param=None):
+        self.col = {}
